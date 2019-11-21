539ffad8e94921803ff3cf75312744ff6c18f268
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-11-20 19:03:06 -0800

    connects to db

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+const db = config.get('mongoURI')
+const options = { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, options)
+        console.log('mongoDB connected...')
+    } catch (err) {
+        console.error(err.message)
+        // exit process with failure
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
