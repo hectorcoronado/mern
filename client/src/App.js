57a846a5cd5ba0cc4a1e057a3048b6256c4bfe9b
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-12-04 05:27:32 -0800

    scaffolds login & register components, sets up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // components
-import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Navbar from './components/layout/Navbar'
+import Register from './components/auth/Register'
 
 // styles
-import './App.css';
+import './App.css'
 
 const App = () =>
-  <>
-    <Landing />
-    <Navbar />
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </section>
+    </>
+  </Router>
 export default App;
