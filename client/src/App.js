153204b0a364a8d36a977136f5702f6a5a981d52
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-12-03 21:57:02 -0800

    scaffolds react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+
+// styles
+import './App.css';
+
+const App = () =>
+  <>
+    <Landing />
+    <Navbar />
+  </>
+export default App;
