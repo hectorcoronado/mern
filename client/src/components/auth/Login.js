a31dd95296ead22be8d8a1362705234703e52a49
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-12-04 17:06:56 -0800

    adds required jsx to Login and Register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-    return (
-        <div>
-            Login
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  /**
+    * in order to use the same `onChange` fn to control all our inputs,
+    * we need to get the value of the `name` attribute of each element
+    * to ascertain that we're only updating the germane state value,
+    * therefore we use `[e.target.name]` as the key
+    */
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    console.log('success');
+  }
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'><i className='fas fa-user'></i> Sign In to Your Account</p>
+      <form className='form' action='create-profile.html' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
         </div>
-    )
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  )
 }
 
 export default Login
