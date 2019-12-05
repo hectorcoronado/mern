91f33d1671263af3cf29f1ecfa4a38c50e363f6a
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-12-05 15:13:44 -0800

    adds redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // components
@@ -10,17 +10,24 @@ import Register from './components/auth/Register'
 // styles
 import './App.css'
 
+// redux
+import { Provider } from 'react-redux'
+import store from './store'
+
 const App = () =>
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+
 export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+// ROOT REDUCER
+import { combineReducers } from 'redux'
+
+/**
+ * @param Object of all reducers we create
+ */
+export default combineReducers({
+
+})
